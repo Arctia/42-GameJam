416cc3579b06994e19202cea8ead9416a071e371
Giorgio Martines <gimartin@e3r4p5.42roma.it>
2023-06-20 12:42:19 +0200

    HUD

---
+++ HUD/PauseButton.gd
@@ -0,0 +1,10 @@
+extends TextureButton
+
+# Called when the node enters the scene tree for the first time.
+func _on_pressed():
+	if get_tree().paused == false:
+		$MenuButton.visible = true
+		get_tree().paused = true
+	elif get_tree().paused == true:
+		$MenuButton.visible = false
+		get_tree().paused = false
